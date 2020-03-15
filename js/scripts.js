c52d3f1216a63cab7a5cc82db1190a8b92726b42
Surya Teja <45654145+surya-space@users.noreply.github.com>
2020-03-16 01:31:12 +0530

    added a modal for event description, updated home page, added navigation when clicking events on home page

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
+    $('#sidebarCollapse').click(function () {
         $('#sidebar').toggleClass('active');
     });
+
+    $('.upcoming-events').click(function(){
+        window.location.href='./events.html'
+    })
+
+
+    $('#eventDetailsModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) ;
+        var title = button.data('title');
+        var date = button.data('date');
+        var modal = $(this);
+        modal.find('#eventTitle').text(title);
+        modal.find('#eventDate').text(date);
+      })
 });
